bda152e6d50d3f4f6504f5783fb1aaf3111783d4
osamu_kondo <tonyhawkingsexample@example.com>
2023-05-30 21:50:02 +0900

    addRotation

--- tetris.js
+++ tetris.js
@@ -23,79 +23,68 @@ class Block {
         context.strokeRect(this.x * Block.size, this.y * Block.size, Block.size, Block.size);
     }
     
-
 }
 
 class Mino {
     constructor(x, y){
         this.x = x;
         this.y = y;
-        this.tetro = Mino.minos[Math.floor(Math.random() * Mino.minos.length)];
+        this.tetro = Mino.tetros[Math.floor(Math.random() * Mino.tetros.length)];
     }
 
     //Minoの各長さ
     static size = 4;
 
-    /*
-    static tetro = [//試しにz型のみ
-        [1,1,0,0],
-        [0,1,1,0],
-        [0,0,0,0],
-        [0,0,0,0]
-    ]
-    */
-
-    static minos =
-    [
+    static tetros = [
       [
-        [0, 1, 0, 0],
-        [1, 1, 1, 0],
-        [0, 0, 0, 0],
-        [0, 0, 0, 0]
+          [0, 0, 0, 0],
+          [0, 1, 1, 0],
+          [0, 0, 1, 1],
+          [0, 0, 0, 0]
       ],
       
       [
-        [0, 0, 1, 0],
-        [0, 0, 1, 0],
-        [0, 0, 1, 0],
-        [0, 0, 1, 0]
-      ],
-    
-      [
-        [1, 0, 0, 0],
-        [1, 1, 1, 0],
-        [0, 0, 0, 0],
-        [0, 0, 0, 0]
-      ],
-    
-      [
-        [0, 0, 1, 0],
-        [1, 1, 1, 0],
-        [0, 0, 0, 0],
-        [0, 0, 0, 0]
-      ],
-    
-      [
-        [1, 1, 0, 0],
-        [1, 1, 0, 0],
-        [0, 0, 0, 0],
-        [0, 0, 0, 0]
-      ],
-    
-      [
-        [1, 1, 0, 0],
-        [0, 1, 1, 0],
-        [0, 0, 0, 0],
-        [0, 0, 0, 0]
-      ],
-    
-      [
-        [0, 1, 1, 0],
-        [1, 1, 0, 0],
-        [0, 0, 0, 0],
-        [0, 0, 0, 0]
-      ]
-    ];
+          [0, 0, 0, 0],
+          [1, 1, 1, 0],
+          [0, 1, 0, 0],
+          [0, 0, 0, 0]
+        ],
+        
+        [
+          [0, 0, 1, 0],
+          [0, 0, 1, 0],
+          [0, 0, 1, 0],
+          [0, 0, 1, 0]
+        ],
+        
+        [
+          [0, 0, 0, 0],
+          [0, 1, 1, 1],
+          [0, 1, 0, 0],
+          [0, 0, 0, 0]
+        ],
+        
+        [
+          [0, 0, 0, 0],
+          [1, 1, 1, 0],
+          [0, 0, 1, 0],
+          [0, 0, 0, 0]
+        ],
+        
+        [
+          [0, 0, 0, 0],
+          [0, 1, 1, 0],
+          [0, 1, 1, 0],
+          [0, 0, 0, 0]
+        ],
+
+        [
+          [0, 0, 0, 0],
+          [0, 0, 1, 1],
+          [0, 1, 1, 0],
+          [0, 0, 0, 0]
+        ]
+      ];
 
 
     //tatroを描写
@@ -115,6 +104,23 @@ class Mino {
             }
         }
     }
+
+    move(dx, dy){
+        this.x += dx;
+        this.y += dy;
+    }
+
+    
+    rotate() {
+        const array = this.tetro;
+        let rotatedArray = array[0].map((_, index) => array.map(row => row[index])).reverse();
+      
+        this.x += Math.round((this.tetro[0].length - this.tetro.length) / 2);
+        this.y += Math.round((this.tetro.length - this.tetro[0].length) / 2);
+        this.tetro = rotatedArray;
+
+    }
+    
 }
 
 class Field {
@@ -131,18 +137,84 @@ class Field {
         canvas.width = Field.canvasW;
         canvas.height = Field.canvasH;
     }
+
+    constructor() {
+        this.field = this.createField();
+    }
+
+    createField() {
+        const field = [];
+        for (let y = 0; y < Field.Row; y++) {
+          const row = [];
+          for (let x = 0; x < Field.Col; x++) {
+            row.push(0);
+          }
+          field.push(row);
+        }
+        return field;
+    }
+
+
+
+    draw() {
+        for (let y = 0; y < Field.Row; y++) {
+          for (let x = 0; x < Field.Col; x++) {
+            if (this.field[y][x] === 1) {
+              const block = new Block(x, y);
+              block.draw();
+            }
+          }
+        }
+      }
+      
 }
 
-    
 
 
 
 
-//ここから記述
+
+
+//ここから記述                                                                            //ボタンで動かす。
 //canvasの大きさを決定
 Field.makeCanvas()
 
+    // main
+    let field = new Field();
+    let mino4 = new Mino(3, 7);
+
+    // 描写
+    function draw(){
+        context.clearRect(0, 0, canvas.width, canvas.height);
+        field.draw();
+        mino4.draw();
+    }
+
+    draw();
+
+    // 回転ボタンのクリックイベント
+    let rotateButton = document.getElementById("rotateButton");
+    rotateButton.addEventListener("click", function(){
+        mino4.rotate();
+        draw();
+    });
+
+    //　右移動のクリックイベント
+    let toRightButton = document.getElementById("moveToRight");
+    toRightButton.addEventListener("click", function(){
+        mino4.move(1, 0);
+        draw();
+    });
+    
+    //　左移動のクリックイベント
+    let toLeftButton = document.getElementById("moveToLeft");
+    toLeftButton.addEventListener("click", function(){
+        mino4.move(-1, 0);
+        draw();
+    });
 //blockを表示
+
+/*
 let block1 = new Block(3,4);
 block1.draw();
 new Block(2,8).draw()
@@ -154,10 +226,11 @@ mino1.draw()
 let mino2 = new Mino(8,0);
 mino2.draw()
 
-let mino4 = new Mino(7,7);
+let mino4 = new Mino(6,7);
 //mino4.draw();//
 mino4.rotate();
 mino4.draw();
+
 
 let mino5 = new Mino(6,12);
 mino5.rotate();
@@ -168,7 +241,7 @@ let mino6 = new Mino(6,17);
 
 let mino7 = new Mino(3,17);
 mino6.draw()
+*/
 
 console.log(Block.windowH);
 console.log(Block.size);
-
